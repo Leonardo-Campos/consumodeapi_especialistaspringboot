b4141f29430bc44b0cd2edd6930d8631b63f6e61
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2022-12-26 20:06:27 -0300

    implementação de consulta de formas de pagamento

--- client.js
+++ client.js
@@ -1,12 +1,29 @@
-function consultarRestaurantes() {
+function consultar() {
     $.ajax({
-        url: "http://api.algafood.local:8080/restaurantes",
-        type: "get",
-
-        success: function(response) {
-            $("#conteudo").text(response);
-        }
+      url: "http://api.algafood.local:8080/formas-pagamento",
+      type: "get",
+  
+      success: function(response) {
+        preencherTabela(response);
+      }
     });
-}
+  }
+  
+  function preencherTabela(formasPagamento) {
+    $("#tabela tbody tr").remove();
+  
+    $.each(formasPagamento, function(i, formaPagamento) {
+      var linha = $("<tr>");
+  
+      linha.append(
+        $("<td>").text(formaPagamento.id),
+        $("<td>").text(formaPagamento.descricao)
+      );
+  
+      linha.appendTo("#tabela");
+    });
+  }
+  
+  
+  $("#btn-consultar").click(consultar);
 
-$("#botao").click(consultarRestaurantes);
